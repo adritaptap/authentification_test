57aae3f0a8a53b8d88dafb8d9650de268dc038d1
adritaptap <adrien.juhem@campus-numerique-in-the-alps.com>
2017-01-25 17:45:07 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>test authentification</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+</head>
+<body>
+	<div class="container content">
+		<h1>Bienvenue sur Le site.</h1>
+		<h2>Le seul, l'unique vous êtes sur Le site.</h2>
+
+		<h3>Connexion</h3>
+		<p>Vous ne pouvez pas vous inscrire, juste vous connecter !!!</p>
+
+		<form method="post" action="">
+			<div class="form-group">
+				<div class="row">
+					<div class="col-md-6 col-lg-4">
+						<label for="pseudo" > Pseudo ou Email </label>
+						<input type="text" name="pseudo" class="form-control">
+
+						<label for="password"> Mot de passe </label>
+						<input type="text" name="password" class="form-control">
+
+						<div class="row">
+						<div class="col-md-offset-5 col-md-7"><br>
+								<button type="submit" class="btn btn-success">Valider</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</form>
+	</div>
+</body>
+</html>
